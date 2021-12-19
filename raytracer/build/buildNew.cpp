6974d39496d15284fb2177e1712eaa3f3e8c1179
abbashaid <60725620+abbashaid@users.noreply.github.com>
2021-12-19 21:35:19 +0500

    latest

--- raytracer/build/buildNew.cpp
+++ raytracer/build/buildNew.cpp
@@ -30,11 +30,11 @@
 
 void
 World::build(void) {
-  vplane.top_left.x = -2;
-  vplane.top_left.y = 2;
+  vplane.top_left.x = -1;
+  vplane.top_left.y = 1;
   vplane.top_left.z = 3;
-  vplane.bottom_right.x = 2;
-  vplane.bottom_right.y = -2;
+  vplane.bottom_right.x = 1;
+  vplane.bottom_right.y = -1;
   vplane.bottom_right.z = 3;
   vplane.hres = 400;
   vplane.vres = 400;
@@ -46,13 +46,22 @@ World::build(void) {
   bg_color = black;  // background color.
 
   // camera and sampler.
-  Camera *cam = new Pinhole(Point3D(0, 0, 10), Point3D(0, 0, -10), 7);
+  Camera *cam = new Pinhole(Point3D(0, 0, 10), Point3D(0, 0, 0), 7);
   cam->compute_uvw();
   set_camera(cam);
   sampler_ptr = new Simple(camera_ptr, &vplane);
   set_acceleration(g_ptr);
 
+
   // load_OBJ("C:\\Users\\abbas\\3D Objects\\Intergalactic_Spaceship-(Wavefront).obj");
-  load_OBJ("C:\\Users\\abbas\\3D Objects\\cube.obj");
+  // load_OBJ("C:\\Users\\abbas\\3D Objects\\cube.obj");
+  load_OBJ("..\\assets\\PokeBall.obj");
+
+  // vertical plane
+  Plane* plane_ptr = new Plane(Point3D(0, 0, -150), Vector3D(0, 0, 1));
+  plane_ptr->set_material(new Cosine(green));
+  add_geometry(plane_ptr);
+
+  add_geometry(g_ptr);
 }
 
